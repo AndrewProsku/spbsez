36fd249ca6e74c2d7f81830bab1778a7875c1927
Евгений Петухов <evgeny.petukhov@kelnik.ru>
2019-02-19 16:11:56 +0300

    #55917 создаёт дополнительную главную страницу с другой фоновой анимацией для демонстрации заказчику

--- src/components/background-animation/index.js
+++ src/components/background-animation/index.js
@@ -1,15 +1,22 @@
 import 'particles.js';
 
 const bgAnimation = document.querySelector('#j-particles');
+const bgAnimationv2 = document.querySelector('#j-particles_v2');
 
-if (bgAnimation) {
+const animateBackground = function(tag, json) {
     const diameter = 240;
     const radius = 120;
 
-    window.particlesJS.load('j-particles', '../assets/particles.json');
+    window.particlesJS.load(tag, json);
 
 
     window.addEventListener('mousemove', (event) => {
         bgAnimation.style['clip-path'] = `circle(${diameter}px at ${event.screenX}px ${event.screenY - radius}px)`;
     });
+};
+
+if (bgAnimation) {
+    animateBackground('j-particles', '../assets/particles.json');
+} else if (bgAnimationv2) {
+    animateBackground('j-particles_v2', '../assets/particles_v2.json');
 }
