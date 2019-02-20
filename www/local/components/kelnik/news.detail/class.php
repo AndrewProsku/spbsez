27dd7c7c9da858029fa756f32613283a5b09d13d
Aleksandr Krutov <plastique.a@gmail.com>
2019-02-20 19:03:55 +0300

    #56764 UPD: media news page

--- www/local/components/kelnik/news.detail/class.php
+++ www/local/components/kelnik/news.detail/class.php
@@ -7,6 +7,8 @@ use Kelnik\Helpers\ArrayHelper;
 use Kelnik\News\News\ImageToNewsTable;
 use Kelnik\News\News\NewsTable;
 use Kelnik\Helpers\BitrixHelper;
+use Kelnik\News\News\TagsTable;
+use Kelnik\Refbook\Model\PresTable;
 
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     die();
@@ -62,21 +64,56 @@ class NewsDetail extends Bbc\Basis
             ]
         ])->fetchAll();
 
-        $element['IMAGES'] = ArrayHelper::getColumn(
-            BitrixHelper::prepareFileFields($element['IMAGES'], ['VALUE']),
-            'VALUE'
-        );
+        if ($element['IMAGES']) {
+            $element['IMAGES'] = \Kelnik\Helpers\BitrixHelper::prepareFileFields(
+                $element['IMAGES'],
+                [
+                    'VALUE' => 'full'
+                ]
+            );
 
-        $element = BitrixHelper::prepareFileFields($element, ['IMAGE', 'IMAGE_PREVIEW']);
-
-        if ($arElement = $this->processingElementsResult($element)) {
-            $this->arResult = array_merge($this->arResult, $arElement);
+            foreach ($element['IMAGES'] as &$v) {
+                $v = [
+                    'SRC' => \CFile::GetFileSRC($v['VALUE']),
+                    'DESCRIPTION' => $v['VALUE']['DESCRIPTION']
+                ];
+            }
         }
 
-        $this->arResult['SEO_TAGS']['TITLE'] = htmlentities($arElement['NAME'], ENT_QUOTES, 'UTF-8');
+        $element['TAGS'] = TagsTable::getList([
+            'select' => [
+                'ID', 'NAME'
+            ],
+            'filter' => [
+                '=ACTIVE' => TagsTable::YES,
+                '=NEWS_TAG.ENTITY_ID' => $element['ID']
+            ],
+            'group' => [
+                'ID'
+            ],
+            'order' => [
+                'SORT' => 'ASC',
+                'NAME' => 'ASC'
+            ]
+        ])->fetchAll();
+
+        $element['TAGS'] = TagsTable::prepareTags(
+            $element['TAGS'],
+            ArrayHelper::getValue($this->arParams, 'SEF_FOLDER', '')
+        );
+
+        if ($element['DATE_SHOW'] instanceof \Bitrix\Main\Type\DateTime) {
+            $element['DATE_SHOW_FORMAT'] = mb_strtolower(FormatDate('j F', $element['DATE_SHOW']));
+
+            if ($element['DATE_SHOW']->format('Y') != date('Y')) {
+                $element['DATE_SHOW_FORMAT'] .= ' ' . $element['DATE_SHOW']->format('Y');
+            }
+        }
+
+        $this->arResult = $element;
+        $this->arResult['SEO_TAGS']['TITLE'] = htmlentities($element['NAME'], ENT_QUOTES, 'UTF-8');
 
         $this->setSeoTags();
-
         $this->setResultCacheKeys([
             'ID',
             'CODE',
--- www/media/news/index.php
+++ www/media/news/index.php
@@ -4,6 +4,29 @@ $APPLICATION->SetPageProperty('title', "Новости | АООЭЗ");
 $APPLICATION->SetTitle("Новости");
 ?>
 
-
+<?$APPLICATION->IncludeComponent(
+    "kelnik:news",
+    "media",
+    Array(
+        "AJAX_COMPONENT_ID" => "",
+        "AJAX_TEMPLATE_PAGE" => "",
+        "AJAX_TYPE" => "DEFAULT",
+        "CACHE_GROUPS" => "N",
+        "CACHE_TIME" => "3600",
+        "CACHE_TYPE" => "A",
+        "ELEMENTS_COUNT" => "6",
+        "SECTION_ID" => "1",
+        "SEF_FOLDER" => "/media/news/",
+        "SEF_MODE" => "Y",
+        "SEF_URL_TEMPLATES" => Array("detail"=>"#ELEMENT_CODE#/","index"=>"","section"=>""),
+        "SET_404" => "Y",
+        "SET_SEO_TAGS" => "Y",
+        "SORT_BY_1" => "DATE_SHOW",
+        "SORT_ORDER_1" => "DESC",
+        "SORT_BY_2" => "ID",
+        "SORT_ORDER_2" => "ASC",
+        "USE_AJAX" => "N"
+    )
+);?>
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
