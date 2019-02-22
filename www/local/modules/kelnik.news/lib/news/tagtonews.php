4c215cd5ac2f2981109b1fe5f37d6e93a608ed06
Aleksandr Krutov <aleksandr.krutov@kelnik.ru>
2019-02-22 12:02:52 +0300

    #56764 UPD: news list

--- www/local/modules/kelnik.news/lib/news/tagtonews.php
+++ www/local/modules/kelnik.news/lib/news/tagtonews.php
@@ -3,6 +3,7 @@
 namespace Kelnik\News\News;
 
 use Bitrix\Main\Entity\IntegerField;
+use Bitrix\Main\Entity\ReferenceField;
 use Kelnik\Helpers\Database\DataManager;
 
 class TagToNewsTable extends DataManager
@@ -24,6 +25,14 @@ class TagToNewsTable extends DataManager
             ),
             new IntegerField('ENTITY_ID'),
             new IntegerField('VALUE'),
+
+            new ReferenceField(
+                'NEWS',
+                NewsTable::class,
+                [
+                    'this.ENTITY_ID' => 'ref.ID'
+                ]
+            )
         ];
     }
 }
