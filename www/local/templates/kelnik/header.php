3ce3b1beb95f415c62681af2f64a60952999a76f
Arstan <arstan.seilov@kelnik.ru>
2019-03-15 17:21:36 +0300

    #57763 Добавил метрики в header.php

--- www/local/templates/kelnik/header.php
+++ www/local/templates/kelnik/header.php
@@ -29,6 +29,30 @@ define("PATH_TO_404", '/404.php'); ?>
         $APPLICATION->ShowHead();
         \Bitrix\Main\Localization\Loc::loadMessages(__DIR__ . DIRECTORY_SEPARATOR . 'kelnik.php');
     ?>
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        ym(52811566, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/52811566" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136324578-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-136324578-1');
+    </script>
+
 </head>
 <body>
     <div id="panel"><?$APPLICATION->ShowPanel();?></div>
