640866b6bbcfe8964c74ee5fdd90ca902e3c8446
Aleksandr Krutov <aleksandr.krutov@kelnik.ru>
2019-03-19 18:03:39 +0300

    #57823 UDP: init

--- www/local/php_interface/init.php
+++ www/local/php_interface/init.php
@@ -4,7 +4,8 @@ include realpath(__DIR__ . '/../../../vendor/autoload.php');
 
 AddEventHandler("main", "OnBeforeEventAdd", "OnBeforeEventAddHandler");
 
-function OnBeforeEventAddHandler(&$event, &$lid, $arFields) {
+function OnBeforeEventAddHandler(&$event, &$lid, $arFields)
+{
     if ($event == "FAVORITES") {
         require_once realpath(__DIR__ . '/lib/mail_attach/mail_attach.php');
         SendAttache($event, $lid, $arFields, "/upload/pdf/{$arFields['FILE_NAME']}");
@@ -16,6 +17,9 @@ function OnBeforeEventAddHandler(&$event, &$lid, $arFields) {
 if (!function_exists('getSiteBaseUrl')) {
     function getSiteBaseUrl()
     {
-        return (IsHTTPS() ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
+        return empty($_SERVER)
+            ? ''
+            : (($_SERVER['SERVER_PORT'] == 443 || strtolower($_SERVER['HTTPS']) == 'on') ? 'https' : 'http') .
+                '://' . $_SERVER['HTTP_HOST'];
     }
 }
