9e6b97a18f6f1ef0efbb1a2efa91d4c65fdd2c19
Aleksandr Krutov <aleksandr.krutov@kelnik.ru>
2019-03-19 16:25:44 +0300

    #57823 UPD: fix JS, cache components

--- www/404.php
+++ www/404.php
@@ -9,8 +9,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle(\Bitrix\Main\Localization\Loc::getMessage('KELNIK_TMPL_404'));
 $APPLICATION->SetPageProperty('title', \Bitrix\Main\Localization\Loc::getMessage('KELNIK_TMPL_404'));
 $APPLICATION->SetPageProperty('mainAdditionalClass', 'l-layout__content-inner');
-
-$APPLICATION->IncludeFile('inc_animation.php'); ?>
+?>
 
 <div class="l-404">
     <div class="b-title b-404-title">
