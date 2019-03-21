96abe41362e85b58da05de29ea2250f64a442ae2
Aleksandr Krutov <plastique.a@gmail.com>
2019-03-22 00:49:03 +0300

    NOTASK: add CH lang

--- www/ch/index.php
+++ www/ch/index.php
@@ -1,50 +1,42 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetPageProperty("title", "Main page | SEZ");
+$APPLICATION->SetPageProperty("title", "經濟特區 | SEZ");
 $APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y");
-$APPLICATION->SetTitle("Main page");
+$APPLICATION->SetTitle("經濟特區");
 ?>
-    <div class="l-home__main-screen j-home__main-screen">
-        <? $APPLICATION->IncludeComponent(
-            'kelnik:site.info',
-            'main-video',
-            array(
-                "COMPONENT_TEMPLATE" => "main-video",
-                "CACHE_TYPE" => "A",
-                "CACHE_TIME" => "360000"
-            ),
-            array()
 
-        ); ?>
-        <? $APPLICATION->IncludeComponent(
-            'kelnik:site.info',
-            'social',
-            array(
-                "COMPONENT_TEMPLATE" => "social",
-                "CACHE_TYPE" => "A",
-                "CACHE_TIME" => "360000"
-            ),
-            array()
+<div class="l-about">
+    <div class="b-title b-about-title">
+        <h1><? $APPLICATION->ShowTitle(false); ?></h1>
+    </div>
 
-        ); ?>
-        <div class="b-main-screen-content">
-            <div class="b-main-screen-text">
-            </div>
+    <div class="b-about-desc">
+        <div class="b-about-desc__img">
+            <img src="/images/about/about_UK-mission.png" alt="">
+        </div>
+        <div class="b-about-desc__content">
+            <?$APPLICATION->IncludeComponent(
+                "kelnik:textblocks",
+                "",
+                Array(
+                    "CACHE_TIME" => "360000",
+                    "CACHE_TYPE" => "A",
+                    "CODE" => "about_block_ch_1"
+                )
+            );?>
         </div>
     </div>
-    <div class="l-home__block_bg_fixed">
-        <? $APPLICATION->IncludeComponent(
-            "kelnik:refbook.list",
-            "partners",
-            array(
-                "COMPONENT_TEMPLATE" => "partners",
-                "SECTION" => "1",
-                "CACHE_GROUPS" => "N",
+    <div class="l-investors__advantage" id="advantage">
+        <?$APPLICATION->IncludeComponent(
+            "kelnik:textblocks",
+            "",
+            Array(
+                "CACHE_TIME" => "360000",
                 "CACHE_TYPE" => "A",
-                "CACHE_TIME" => "3600"
-            ),
-            array()
-        ); ?>
+                "CODE" => "about_block_ch_2"
+            )
+        );?>
     </div>
+</div>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
