4a832904e5fff58cde7151dcfb012deae0cb68bb
Anastasia Verkhoturova <anastasia.verkhoturova@kelnik.ru>
2019-04-08 18:00:39 +0300

    #58348: выделяет подчеркиванием активный пункт меню

--- www/local/templates/kelnik/components/bitrix/menu/mobile/template.php
+++ www/local/templates/kelnik/components/bitrix/menu/mobile/template.php
@@ -5,13 +5,19 @@
         <?php foreach($arResult as $arItem): ?>
             <div class="b-mobile-menu__nav-block">
                 <div class="b-mobile-menu__nav-header<?php if(!empty($arItem['CHILD'])): ?> b-mobile-menu__nav-header_action_accordion j-mobile-menu-accordion<?php endif; ?>">
-                    <a href="<?= $arItem['LINK'] ?>" class="b-mobile-menu__nav-header-link  b-link-line-two"><?= $arItem['TEXT']; ?></a>
+                    <a href="<?= $arItem['LINK'] ?>"
+                       class="b-mobile-menu__nav-header-link b-link-line-two<?php if(!empty($arItem['SELECTED'])): ?> is-active<?php endif; ?>">
+                        <?= $arItem['TEXT']; ?>
+                    </a>
                 </div>
                 <?php if(!empty($arItem['CHILD'])): ?>
                     <ul class="b-mobile-menu__nav-body">
                     <?php foreach($arItem['CHILD'] as $child): ?>
                         <li class="b-mobile-menu__nav-item">
-                            <a href="<?= $child['LINK']; ?>" class="b-mobile-menu__nav-link"><?= $child['TEXT']; ?></a>
+                            <a href="<?= $child['LINK']; ?>"
+                               class="b-mobile-menu__nav-link<?php if(!empty($child['SELECTED'])): ?> is-active<?php endif; ?>">
+                                <?= $child['TEXT']; ?>
+                            </a>
                         </li>
                     <?php endforeach; ?>
                     </ul>
