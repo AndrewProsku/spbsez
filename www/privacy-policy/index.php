bb92be74c7faf5d0a1df53edc77490d433835ad5
Aleksandr Krutov <aleksandr.krutov@kelnik.ru>
2019-04-18 18:16:06 +0300

    #59077 ADD: pp page

---
+++ www/privacy-policy/index.php
@@ -0,0 +1,20 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Политика конфиденциальности");
+$APPLICATION->SetPageProperty('title', 'Политика конфиденциальности');
+?>
+<div class="l-personal-data">
+    <div class="l-personal-data__title b-title"><h1><? $APPLICATION->ShowTitle(false); ?></h1></div>
+    <div class="l-personal-data__content">
+        <? $APPLICATION->IncludeComponent(
+            "kelnik:textblocks",
+            "",
+            Array(
+                "CACHE_TIME" => "360000",
+                "CACHE_TYPE" => "A",
+                "CODE" => "pp_block_1"
+            )
+        ); ?>
+    </div>
+</div>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
