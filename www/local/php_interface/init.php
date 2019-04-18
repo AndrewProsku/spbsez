109da7fa5a914f3eaeb63186801cab89b882189f
Aleksandr Krutov <aleksandr.krutov@kelnik.ru>
2019-04-18 17:49:42 +0300

    #59009 UPD: rename fields

--- www/local/php_interface/init.php
+++ www/local/php_interface/init.php
@@ -3,22 +3,6 @@
 include realpath(__DIR__ . '/../../../vendor/autoload.php');
 
 \Bitrix\Main\EventManager::getInstance()->addEventHandler("main", "OnBeforeEventAdd", "OnBeforeEventAddHandler");
-\Bitrix\Main\EventManager::getInstance()->addEventHandler(
-    'main',
-    'OnUserDelete',
-    [
-        SezHandler::class,
-        'onUserDelete'
-    ]
-);
-\Bitrix\Main\EventManager::getInstance()->addEventHandler(
-    'main',
-    'OnBeforeUserDelete',
-    [
-        SezHandler::class,
-        'onBeforeUserDelete'
-    ]
-);
 
 function OnBeforeEventAddHandler(&$event, &$lid, $arFields)
 {
@@ -37,49 +21,6 @@ if (!function_exists('getSiteBaseUrl')) {
             ? ''
             : (($_SERVER['SERVER_PORT'] == 443 || strtolower($_SERVER['HTTPS']) == 'on') ? 'https' : 'http') .
                 '://' . $_SERVER['HTTP_HOST'];
-    }
-}
-
-class SezHandler
-{
-    public static function onBeforeUserDelete($userId)
-    {
-        global $APPLICATION, $USER;
-
-        if (!\Bitrix\Main\Loader::includeModule('kelnik.userdata')) {
-            return true;
-        }
-
-        $userGroups = CUser::GetUserGroup($USER->GetID());
-
-        if (!$userGroups || !in_array(\Kelnik\Userdata\Profile\Profile::GROUP_SUPER_ADMIN, $userGroups)) {
-            $APPLICATION->throwException('Недостаточно прав для удаления учетной записи');
-
-            return false;
-        }
-    }
-
-    public static function onUserDelete($userId)
-    {
-        if (!\Bitrix\Main\Loader::includeModule('kelnik.messages')) {
-            return true;
-        }
-
-        $dbConn = \Bitrix\Main\Application::getConnection();
-
-        $tables = [
-            \Kelnik\Messages\Model\MessagesTable::getTableName(),
-            \Kelnik\Messages\Model\NotifyTable::getTableName()
-        ];
-
-        try {
-            foreach ($tables as $table) {
-                $dbConn->query('DELETE FROM `' . $table . '` WHERE `USER_ID` = ' . $dbConn->getSqlHelper()->convertToDbInteger($userId));
-            }
-        } catch (Exception $e) {
-        }
-
-        return true;
     }
 }
 
