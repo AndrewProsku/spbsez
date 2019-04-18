9ccfeadd8ebc452d8857b7609137f28cf0da9bbd
Sergey Kondrashev <sergey.kondrashev@kelnik.ru>
2019-04-18 16:09:29 +0300

    #58963 Добавлена страница "Политика конфиденциальности"

--- www/local/templates/kelnik/footer.php
+++ www/local/templates/kelnik/footer.php
@@ -23,6 +23,7 @@
                 <span class="b-footer__copyright-text">© <?= date('Y'); ?>, <?= \Bitrix\Main\Localization\Loc::getMessage('KELNIK_TMPL_FOOTER_SEZ'); ?></span>
                 <?php if(LANG_DIR == \SezLang::RUSSIAN_DIR): ?>
                     <a href="<?= LANG_DIR . 'about/info/'; ?>" class="b-footer__copyright-link">Раскрытие информации</a>
+                    <a href="#" class="b-footer__copyright-link">Политика конфиденциальности</a>
                 <?php endif; ?>
                 <button class="b-footer__copyright-link j-message-button" data-href="#message">
                     <?= \Bitrix\Main\Localization\Loc::getMessage('KELNIK_TMPL_WRITE_MESSAGES'); ?>
