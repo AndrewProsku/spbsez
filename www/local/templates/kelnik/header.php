d519a9e8638026de95ae3fe61ba2af750a44e890
aleksandr.kovalevich <aleksandr.kovalevich@kelnik.ru>
2020-01-14 13:42:38 +0300

    #66245 - изменил код для яндекс-метрики

--- www/local/templates/kelnik/header.php
+++ www/local/templates/kelnik/header.php
@@ -32,14 +32,16 @@ define("PATH_TO_404", '/404.php');
     <?php $APPLICATION->ShowHead(); ?>
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-        ym(52811566, "init", {
+       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+       m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+       ym(52811566, "init", {
             clickmap:true,
             trackLinks:true,
-            accurateTrackBounce:true
-        });
+            accurateTrackBounce:true,
+            webvisor:true
+       });
     </script>
     <noscript><div><img src="https://mc.yandex.ru/watch/52811566" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
