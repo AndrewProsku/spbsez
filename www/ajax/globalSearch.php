6e6eb5b97298cf64c5807d1d4b7a042b8266e3f3
Anton Arkhipov <anton.arkhipov@kelnik.ru>
2020-03-13 19:05:18 +0300

    66323 - search

--- www/ajax/globalSearch.php
+++ www/ajax/globalSearch.php
@@ -46,7 +46,7 @@ class Search
         $this->needle = $this->request['q'];
 
         if ($this->request['type']) {
-            $this->type = 'search'.$this->request['type'];
+            $this->type = 'search' . $this->request['type'];
         }
     }
 
@@ -82,10 +82,10 @@ class Search
                 'page' => 'news',
                 'section' => $oneNews['KELNIK_NEWS_NEWS_NEWS_CAT_NAME'],
                 'NAME' => $oneNews['NAME'],
-                'LINK' => $_SERVER['HTTP_ORIGIN'].'/media/news/'.$oneNews['CODE'].'/'
+                'LINK' => $_SERVER['HTTP_ORIGIN'] . '/media/news/' . $oneNews['CODE'] . '/'
             ];
             if ($count >= 5 && !$this->type) {
-                $this->json['data'][$this->key]['linkMore'] = '/search/?type=News&q='.$this->needle;
+                $this->json['data'][$this->key]['linkMore'] = '/search/?type=News&q=' . $this->needle;
                 $this->key++;
                 break;
             }
@@ -96,7 +96,7 @@ class Search
     {
         $residents = Kelnik\Text\Blocks\BlocksTable::getList(
             [
-                'select' => ['ID', 'ACTIVE', 'CATEGORY','BODY'],
+                'select' => ['ID', 'ACTIVE', 'CATEGORY', 'BODY'],
                 'filter' => [
                     '%BODY' => $this->needle,
                     'ACTIVE' => 'Y'
@@ -108,13 +108,42 @@ class Search
         )->FetchAll();
 
         foreach ($residents as $count => $oneResident) {
-//            $strings = preg_split('/\. |\\n|\\r|\\t|, /', $oneResident['BODY'],-1,PREG_SPLIT_NO_EMPTY);
-            $name = preg_match("/([^\\n\\t\\r,]+{$this->needle}[^\\n\\t\\r\.]+)/", $oneResident['BODY'], $matches);
-//            $name = preg_match("/{$this->needle}/', $oneResident['BODY'], $matches);
+            $text = strip_tags($oneResident['BODY']);
+            $text = str_replace('&nbsp;', ' ', $text);
+            $position = stripos($text, $this->needle);
+            $start = $position - 20;
+            $lenght = 70;
+            if ($start < 0) {
+                $start = 0;
+                $lenght = $lenght - $start;
+            }
+            $text = substr($text, $start, $lenght);
+            $position = stripos($text, $this->needle);
+            $first = substr($text, 0, 1);
+            if (!($position <= 2 || $first === strtoupper($first))) {
+                $text = ltrim($text);
+                $position = stripos($text, ' ');
+                $text = substr($text, $position + 1);
+            }
+            $end = substr($text, -1);
+            if (strtolower($end) !== strtoupper($end)) {
+                $textArray = explode(' ', $text);
+                array_pop($textArray);
+                $text = implode(' ', $textArray);
+            }
+            $first = substr($text, 0, 1);
+            if ($first !== strtoupper($first)) {
+                $text = '...' . $text;
+            }
+            if (!in_array(substr($text, -1), ['!', '.', '?', ';', ':'])) {
+                $text .= '...';
+            }
+
+
             $this->json['data'][$this->key]['items'] [] = [
                 'page' => 'news',
                 'section' => $oneResident['KELNIK_TEXT_BLOCKS_BLOCKS_CATEGORY_TITLE'],
-                'NAME' => $matches[0],
+                'NAME' => $text,
                 'LINK' => $_SERVER['HTTP_ORIGIN'] . $oneResident['KELNIK_TEXT_BLOCKS_BLOCKS_CATEGORY_ALIAS'] . '/'
             ];
             if ($count >= 5 && !$this->type) {
@@ -157,7 +186,7 @@ class Search
                 'LINK' => CFile::GetFileArray($doc['FILE_ID'])['SRC']
             ];
             if ($count >= 5 && !$this->type) {
-                $this->json['data'][$this->key]['linkMore'] = '/search/?type=Docs&q='.$this->needle;
+                $this->json['data'][$this->key]['linkMore'] = '/search/?type=Docs&q=' . $this->needle;
                 $this->key++;
                 break;
             }
