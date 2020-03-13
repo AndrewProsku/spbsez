a42f8552f3ebe264592118e000ccf138562e8d19
Anton Arkhipov <anton.arkhipov@kelnik.ru>
2020-03-13 16:58:12 +0300

    66323 - search

--- www/ajax/globalSearch.php
+++ www/ajax/globalSearch.php
@@ -26,83 +26,124 @@ foreach ($requiredModules as $requiredModule) {
 
 $json = BitrixHelper::getDefaultJson();
 
-class Search {
+class Search
+{
 
     private $request;
-    private $filter;
+    private $needle;
     public $json;
     private $type;
     private $key = 0;
 
-    function __construct($req){
+    public function __construct($req)
+    {
         $this->request = $req;
         $this->json = BitrixHelper::getDefaultJson();
-        if(!$this->request) {
+        if (!$this->request) {
             $this->json['status'] = 0;
             $this->json['errors'][] = 'Нет строки поиска';
         }
-        $this->filter = array('%NAME' => $req['q']);
+        $this->needle = $this->request['q'];
 
-        if($this->request['type']) {
+        if ($this->request['type']) {
             $this->type = 'search'.$this->request['type'];
         }
     }
 
-    public function doSearch(){
-        if($this->type) {
-            if(method_exists($this, $this->type)){
+    public function doSearch()
+    {
+        if ($this->type) {
+            if (method_exists($this, $this->type)) {
                 $this->{$this->type}();
             }
             return;
-        }
-        else{
+        } else {
+            $this->searchTextBlocks();
             $this->searchNews();
             $this->searchDocs();
         }
         BitrixHelper::jsonResponse($this->json);
     }
 
-    public function searchNews(){
+    private function searchNews()
+    {
         $news = NewsTable::getList(
             [
-                'select' => ['ID', 'NAME', 'CAT', 'CODE'],
-                'filter' => $this->filter,
+                'select' => ['ID', 'NAME', 'CAT', 'CODE', 'TEXT', 'TEXT'],
+                'filter' => ['%TEXT' => $this->needle],
                 'group' => ['ID'],
-                'order' => ['ID' => 'desc']
+                'order' => ['ID' => 'desc'],
+                'limit' => 5
             ]
         )->FetchAll();
 
-        foreach ($news as $count => $oneNews){
+        foreach ($news as $count => $oneNews) {
             $this->json['data'][$this->key]['items'] [] = [
                 'page' => 'news',
                 'section' => $oneNews['KELNIK_NEWS_NEWS_NEWS_CAT_NAME'],
                 'NAME' => $oneNews['NAME'],
                 'LINK' => $_SERVER['HTTP_ORIGIN'].'/media/news/'.$oneNews['CODE'].'/'
             ];
-            if($count >= 2 && !$this->type) {
-                $this->json['data'][$this->key]['linkMore'] = '/search/?type=News&q='.$_REQUEST['q'];
+            if ($count >= 5 && !$this->type) {
+                $this->json['data'][$this->key]['linkMore'] = '/search/?type=News&q='.$this->needle;
                 $this->key++;
                 break;
             }
         }
     }
 
-    public function searchDocs(){
+    private function searchTextBlocks()
+    {
+        $residents = Kelnik\Text\Blocks\BlocksTable::getList(
+            [
+                'select' => ['ID', 'ACTIVE', 'CATEGORY','BODY'],
+                'filter' => [
+                    '%BODY' => $this->needle,
+                    'ACTIVE' => 'Y'
+                ],
+                'group' => ['ID'],
+                'order' => ['ID' => 'desc'],
+                'limit' => 5
+            ]
+        )->FetchAll();
+
+        foreach ($residents as $count => $oneResident) {
+//            $strings = preg_split('/\. |\\n|\\r|\\t|, /', $oneResident['BODY'],-1,PREG_SPLIT_NO_EMPTY);
+            $name = preg_match("/([^\\n\\t\\r,]+{$this->needle}[^\\n\\t\\r\.]+)/", $oneResident['BODY'], $matches);
+//            $name = preg_match("/{$this->needle}/', $oneResident['BODY'], $matches);
+            $this->json['data'][$this->key]['items'] [] = [
+                'page' => 'news',
+                'section' => $oneResident['KELNIK_TEXT_BLOCKS_BLOCKS_CATEGORY_TITLE'],
+                'NAME' => $matches[0],
+                'LINK' => $_SERVER['HTTP_ORIGIN'] . $oneResident['KELNIK_TEXT_BLOCKS_BLOCKS_CATEGORY_ALIAS'] . '/'
+            ];
+            if ($count >= 5 && !$this->type) {
+                $this->json['data'][$this->key]['linkMore'] = '/search/?type=News&q=' . $this->needle;
+                $this->key++;
+                break;
+            }
+        }
+    }
+
+    private function searchDocs()
+    {
         $docs = Kelnik\Refbook\Model\DocsTable::getList(
             [
                 'select' => ['ID', 'NAME', 'FILE_ID'],
-                'filter' => $this->filter,
+                'filter' => ['%NAME' => $this->needle],
                 'group' => ['ID'],
-                'order' => ['ID' => 'desc']
+                'order' => ['ID' => 'desc'],
+                'limit' => 5
             ]
         )->fetchAll();
 
         $infoDocs = Kelnik\Info\Model\DocsTable::getList(
             [
                 'select' => ['ID', 'NAME', 'FILE_ID'],
-                'filter' => $this->filter,
+                'filter' => ['%NAME' => $this->needle],
                 'group' => ['ID'],
-                'order' => ['ID' => 'desc']
+                'order' => ['ID' => 'desc'],
+                'limit' => 5
             ]
         )->fetchAll();
 
@@ -115,8 +156,8 @@ class Search {
                 'NAME' => $doc['NAME'],
                 'LINK' => CFile::GetFileArray($doc['FILE_ID'])['SRC']
             ];
-            if($count >= 2 && !$this->type) {
-                $this->json['data'][$this->key]['linkMore'] = '/search/?type=Docs&q='.$_REQUEST['q'];
+            if ($count >= 5 && !$this->type) {
+                $this->json['data'][$this->key]['linkMore'] = '/search/?type=Docs&q='.$this->needle;
                 $this->key++;
                 break;
             }
