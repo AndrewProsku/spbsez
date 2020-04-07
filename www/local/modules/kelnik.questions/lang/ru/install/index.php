1afb9bb0dc931c2c20a8fd3fdd9a2d1e7495815e
Anton Arkhipov <anton.arkhipov@kelnik.ru>
2020-04-07 17:34:12 +0300

    Revert "Revert "Merge branch 'revert-e3426ad4' into 'dev'""
    This reverts commit 17e5f5b3

---
+++ www/local/modules/kelnik.questions/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$MESS['KELNIK_QUESTIONS_COMPLETE_NAME'] = 'Вопрос-Ответ';
+$MESS['KELNIK_QUESTIONS_COMPLETE_DESCRIPTION'] = '';
+$MESS['KELNIK_QUESTIONS_PARTNER_NAME'] = 'Kelnik Studios';
+$MESS['KELNIK_REQUIRE_MODULES'] = 'Не установлены зависимости модуля.<br>Требуемые модули: <strong>#MODULES#</strong>.<br>Установите требуемые зависимости и попробуйте снова.';
