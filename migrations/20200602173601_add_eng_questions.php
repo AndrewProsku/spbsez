c382cdd1a8d536fa72ba972b84bc5310e9d6d0a8
Roman <kuzmin.sto@yandex.ru>
2020-06-02 18:32:56 +0300

    #68492 FIX: Обновляет миграцию по добавлению поля языка в таблицу популярных запросов

--- migrations/20200602173601_add_eng_questions.php
+++ migrations/20200602173601_add_eng_questions.php
@@ -7,13 +7,16 @@ class AddEngQuestions extends AbstractMigration
 {
     public function change()
     {
-       $this->execute('ALTER TABLE kelnik_questions ADD LANG CHAR(2) NULL');
-       $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Become a resident", "https://www.spbsez.ru/en/investors/#investors-resident", "html", "en")');
-       $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Data Center Services", "https://www.spbsez.ru/en/services/#data-services-center", "html", "en")');
-       $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Rental of meeting rooms", "https://www.spbsez.ru/en/services/#rental-of-meeting-rooms", "html", "en")');
-       $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Tariffs for the services of the Management Company", "https://www.spbsez.ru/en/services/", "html", "en")');
-       $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Benefits and preferences", "https://www.spbsez.ru/en/#benefits-and-preferences", "html", "en")');
-    }
+        $table = $this->table('kelnik_questions');
 
+        if (!$table->hasColumn('LANG')) {
+            $table->addColumn('LANG', 'string', ['limit' => 2, 'null' => true])->update();
+            $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Become a resident", "https://www.spbsez.ru/en/investors/#investors-resident", "html", "en")');
+            $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Data Center Services", "https://www.spbsez.ru/en/services/#data-services-center", "html", "en")');
+            $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Rental of meeting rooms", "https://www.spbsez.ru/en/services/#rental-of-meeting-rooms", "html", "en")');
+            $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Tariffs for the services of the Management Company", "https://www.spbsez.ru/en/services/", "html", "en")');
+            $this->execute('INSERT INTO kelnik_questions(SORT, ACTIVE, NAME, URL, TEXT_TEXT_TYPE, LANG) VALUES(500, "Y", "Benefits and preferences", "https://www.spbsez.ru/en/#benefits-and-preferences", "html", "en")');
+        }
+    }
 }
 
