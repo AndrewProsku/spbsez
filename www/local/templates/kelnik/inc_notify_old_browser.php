0fdd840f5ef5cecb58a6d239e5b3b748ff8f3477
Andrew Pros <vslsitedev@gmail.com>
2024-03-13 12:46:02 +0400

    исправляет заголовки в выдаче

--- www/local/templates/kelnik/inc_notify_old_browser.php
+++ www/local/templates/kelnik/inc_notify_old_browser.php
@@ -3,8 +3,8 @@
     <div class="b-alert-old-browser__content">
         <button class="b-alert-old-browser__close"><!--[if IE 8 ]><img src="/images/alert-old-browser/close.png" alt="close"><![endif]--><!--[if (gt IE 9)|!(IE)]><!--><img src="/images/alert-old-browser/close.svg" alt="close"><!--<![endif]--></button>
         <div class="b-alert-old-browser__icon"><!--[if IE 8 ]><img src="/images/alert-old-browser/bike.png" alt="bike"><![endif]--><!--[if (gt IE 9)|!(IE)]><!--><img src="/images/alert-old-browser/bike.svg" alt="bike"><!--<![endif]--></div>
-        <h2 class="b-alert-old-browser__title"><?= \Bitrix\Main\Localization\Loc::getMessage('KELNIK_TMPL_OLD_BROWSER_HEADER'); ?></h2>
-        <div class="b-alert-old-browser__text"><p><?= \Bitrix\Main\Localization\Loc::getMessage('KELNIK_TMPL_OLD_BROWSER_BODY'); ?></p></div>
+        <!--[if IE 8 ]><h2 class="b-alert-old-browser__title"><?= \Bitrix\Main\Localization\Loc::getMessage('KELNIK_TMPL_OLD_BROWSER_HEADER'); ?></h2><![endif]-->
+        <!--[if IE 8 ]><div class="b-alert-old-browser__text"><p><?= \Bitrix\Main\Localization\Loc::getMessage('KELNIK_TMPL_OLD_BROWSER_BODY'); ?></p></div><![endif]-->
         <ul class="b-alert-old-browser__browsers">
             <li class="b-alert-old-browser__browser"><a class="b-alert-old-browser__browser-link" href="https://browser.yandex.ru/" target="_blank"><!--[if IE 8 ]><img src="/images/alert-old-browser/yandex.png" alt="yandex"><![endif]--><!--[if (gt IE 9)|!(IE)]><!--><img src="/images/alert-old-browser/yandex.svg" alt="yandex"><!--<![endif]--><p class="b-alert-old-browser__browser-name"><?= \Bitrix\Main\Localization\Loc::getMessage('KELNIK_TMPL_OLD_BROWSER_YANDEX'); ?></p></a></li>
             <li class="b-alert-old-browser__browser"><a class="b-alert-old-browser__browser-link" href="https://www.google.ru/chrome/browser/desktop/index.html" target="_blank"><!--[if IE 8 ]><img src="/images/alert-old-browser/chrome.png" alt="chrome"><![endif]--><!--[if (gt IE 9)|!(IE)]><!--><img src="/images/alert-old-browser/chrome.svg" alt="chrome"><!--<![endif]--><p class="b-alert-old-browser__browser-name">Google Chrome</p></a></li>
