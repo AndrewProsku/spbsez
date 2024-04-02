e2cdca98cce6be745a6c69f0c7cb40c90c56ba00
Andrew Pros <vslsitedev@gmail.com>
2024-04-02 11:55:03 +0400

    ссылка с баннера

--- www/local/components/kelnik/banners/templates/simple/template.php
+++ www/local/components/kelnik/banners/templates/simple/template.php
@@ -1,6 +1,6 @@
 <div class="banner_simple-wrapper" data-id="<?=$arResult['ID']?>">
     <div class="banner_simple">
-        <img src="<?=$arResult['IMAGE_PATH']?>">
+        <a href="<?=$arResult['LINK']?>" target="_blank"><img src="<?=$arResult['IMAGE_PATH']?>"></a>
     </div>
 </div>
 
